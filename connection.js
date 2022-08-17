75fd2f0d56671cda1fc2e9724daaa232b0f79d42
Kennedymuthuri <82339780+Kennedymuthuri@users.noreply.github.com>
2022-08-17 15:00:49 +0300

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,6 +2,19 @@ const express=require('express');
 const mongoose=require('mongoose');
 const PORT=8000;
 const multer=require('multer');
-const bodyParser=require('body-parser");
+const bodyParser=require('body-parser');
 const mongoURL='mongodb://0.0.0.0/mern-data';
+const app=express();
+//connecting to the database and server
+mongoose.connect(mongoURL).then(function(){
+  app.listen(PORT,function(error){
+    if(error){
+      console.log('server error:  ',error);
+    }else{
+      console.log(`AApp connected to port ${PORT} and local database`);
+    }
+  });
+}).catch(function(err){
+  console.log('error:  ',err);
+});
                          
