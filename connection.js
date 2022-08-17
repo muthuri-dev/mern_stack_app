13de0e329aa738388cdd45e28762dc1ab4231f97
Kennedymuthuri <82339780+Kennedymuthuri@users.noreply.github.com>
2022-08-17 14:53:29 +0300

    Create connection.js

---
+++ connection.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const PORT=8000;
+const multer=require('multer');
+const bodyParser=require('body-parser");
+const mongoURL='mongodb://0.0.0.0/mern-data';
+                         
