740b2596fc7011b165bf413f095789da42022143
kennedy muthuri <muthurikennedy082@gmail.com>
2022-08-19 07:41:11 +0300

    doing some debuging

--- frontend/src/views/notes.jsx
+++ frontend/src/views/notes.jsx
@@ -1,9 +1,31 @@
-
+import { useEffect, useState } from "react";
 
 const Notes = () => {
+    const [notes,setNotes]=useState([{
+        title:'',
+        note:''
+    }]);
+
+    useEffect(function(){
+        fetch('/notes').then(function(res){
+            if(res.ok){
+                return res.json();
+            }
+        }).then(function(jsonRes){
+            setNotes(jsonRes)
+        }).catch(function(err){
+            console.log('retrieve error:  ',err);
+        })
+    },[]);
     return ( 
         <div className="notes">
             <h2>notes page</h2>
+            {notes.map((note)=>
+            <div>
+                <h1>{note.title}</h1>
+                <p>{note.note}</p>
+            </div>
+            )}
         </div>
      );
 }
